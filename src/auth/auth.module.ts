69ba4b439b3378debeb354e020258d7dea8eb08a
Dera2S <dera.ah.14@gmail.com>
2025-09-18 17:31:35 +0300

    fix : using independant auth repository and separate prisma service logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AuthRepository } from './Auth_repository';
-import { AuthPrismaRepository } from './Auth_Prisma_Repository';
+import {
+  AuthPrismaService,
+  AuthPrismaServiceImpl,
+} from './Auth_Prisma_Repository';
 import { PrismaModule } from '@/prisma/prisma.module';
-import { JwtModule } from '@nestjs/jwt';
+import { AuthRepositoryImpl } from './auth_repositoryImpl';
 
 @Module({
   imports: [PrismaModule, JwtModule.register({})],
   controllers: [AuthController],
   providers: [
     AuthService,
-    { provide: AuthRepository, useClass: AuthPrismaRepository },
+    { provide: AuthRepository, useClass: AuthRepositoryImpl },
+    { provide: AuthPrismaService, useClass: AuthPrismaServiceImpl },
   ],
 })
 export class AuthModule {}
