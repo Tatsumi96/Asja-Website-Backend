67c5b3eb973dad32895daefe0327a8fc361b5bd8
Dera2S <dera.ah.14@gmail.com>
2025-09-18 17:32:06 +0300

    fix : separate prisma service

--- src/auth/Auth_Prisma_Repository.ts
+++ src/auth/Auth_Prisma_Repository.ts
@@ -1,53 +1,53 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
 
-import { failure, Result, success } from '@/core/Result';
 import { LoginDto } from '@/auth/login_Dto';
 import { UserNotFoundException } from './exception';
-import { AuthRepository } from './Auth_repository';
 import { UserEntity } from './user.entity';
 
+export abstract class AuthPrismaService {
+  abstract signIn(userId: number): Promise<LoginDto>;
+  abstract register(user: UserEntity): Promise<void>;
+}
+
 @Injectable()
-export class AuthPrismaRepository implements AuthRepository {
+export class AuthPrismaServiceImpl implements AuthPrismaService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async signIn(userId: number): Promise<Result<LoginDto>> {
+  async signIn(userId: number): Promise<LoginDto> {
     try {
       const user = await this.prisma.student.findFirst({
         where: { Matricule: userId },
         select: { MotDePasse: true },
       });
 
-      if (!user) return failure(new UserNotFoundException());
+      if (!user) throw new UserNotFoundException();
 
-      return success({
+      return {
         identifier: userId,
         password: user.MotDePasse,
-      });
+      };
     } catch (error) {
       console.error(error);
-      return failure(new Error());
+      throw new Error();
     }
   }
 
-  async register(user: UserEntity): Promise<Result<void>> {
+  async register(user: UserEntity): Promise<void> {
     try {
       await this.prisma.student.create({
         data: {
           Matricule: user.matricule,
           Nom: user.name,
           Prenom: user.afterName,
-          Mention: user.mention,
-          Niveau: user.level,
+          Mention: user.mention as string,
+          Niveau: user.level as string,
           MotDePasse: user.password,
         },
       });
-
-      return success(undefined);
     } catch (error) {
       console.error(error);
-
-      return failure(new Error());
+      throw new Error();
     }
   }
 }
