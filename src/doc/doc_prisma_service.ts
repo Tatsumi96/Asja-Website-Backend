2deb973b8e2ae5140bd8179466c6c6f48d91c9e1
Dera2S <dera.ah.14@gmail.com>
2025-09-18 19:39:10 +0300

    fix : adding injectable and throw error

--- src/doc/doc_prisma_service.ts
+++ src/doc/doc_prisma_service.ts
@@ -1,12 +1,16 @@
 import { PrismaService } from '@/prisma/prisma.service';
 import { DocEntity } from './doc.entity';
 
+import { Injectable } from '@nestjs/common';
+
 export abstract class DocPrismaService {
   abstract save(doc: DocEntity): Promise<void>;
 }
 
+@Injectable()
 export class DocPrismaServiceImpl implements DocPrismaService {
   constructor(private readonly prisma: PrismaService) {}
+
   async save(doc: DocEntity): Promise<void> {
     try {
       await this.prisma.document.create({
@@ -19,6 +23,7 @@ export class DocPrismaServiceImpl implements DocPrismaService {
       });
     } catch (error) {
       console.error(error);
+      throw new Error();
     }
   }
 }
