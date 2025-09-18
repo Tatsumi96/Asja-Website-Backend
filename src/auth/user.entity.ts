e31d532f565232e600b7aa4a7adebba752eb24e8
Dera2S <dera.ah.14@gmail.com>
2025-09-18 18:47:03 +0300

    fix : adding role , contact , mention , level and grade

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,27 +1,13 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+export type role = 'Student' | 'Teacher';
 
 export class UserEntity {
-  @IsNumber()
-  @IsNotEmpty()
   matricule: number;
-
-  @IsString()
-  @IsNotEmpty()
   name: string;
-
-  @IsString()
-  @IsNotEmpty()
   afterName: string;
-
-  @IsString()
-  @IsNotEmpty()
-  mention: string;
-
-  @IsString()
-  @IsNotEmpty()
-  level: string;
-
-  @IsString()
-  @IsNotEmpty()
   password: string;
+  contact: string;
+  role: role;
+  mention?: string;
+  level?: string;
+  grade?: string;
 }
