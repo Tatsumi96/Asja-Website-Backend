c6a1b2c31ba644832c1bf0eb581585116f4c88ba
Dera2S <dera.ah.14@gmail.com>
2025-09-19 13:37:30 +0300

    fix : replace return type of auth repository

--- src/auth/Auth_repository.ts
+++ src/auth/Auth_repository.ts
@@ -1,9 +1,9 @@
-import { LoginDto } from '@/auth/login_Dto';
+import { LoginDto, LoginReturnType } from '@/auth/login_Dto';
 import { Result } from '@/core/Result';
 import { UserEntity } from './user.entity';
 
 export abstract class AuthRepository {
-  abstract signIn(login: LoginDto): Promise<Result<LoginDto>>;
+  abstract signIn(login: LoginDto): Promise<Result<LoginReturnType>>;
 
   abstract register(user: UserEntity): Promise<Result<void>>;
 
